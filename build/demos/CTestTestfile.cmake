e1c9dae3d3dc70221474c04f1e5ab6309487fbc7
jeff.li <lijippy@163.com>
2024-07-06 09:33:01 +0800

    init

--- build/demos/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jeff/github/gottingen/eigen/demos
-# Build directory: /home/jeff/github/gottingen/eigen/build/demos
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/unsupported/doc/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jeff/github/gottingen/eigen/unsupported/doc
-# Build directory: /home/jeff/github/gottingen/eigen/build/unsupported/doc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("examples")
-subdirs("snippets")
